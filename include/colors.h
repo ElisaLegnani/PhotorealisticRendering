a06d03ee3820a292a04e668d717edb656946adb0
adelezaini <adele.zaini@yahoo.it>
2021-03-10 11:14:22 +0100

    Add Colors.constructor

---
+++ include/colors.h
@@ -0,0 +1,30 @@
+#ifndef _colors_h_
+#define _colors_h_
+
+class Colors {
+
+  public:
+
+    //Costruttori
+  Colors(){
+    m_r=0.0;
+    m_g=0.0;
+    m_b=0.0;
+  }
+  
+  Colors(float r, float g, float b){
+    m_r=r;
+    m_g=g;
+    m_b=b;
+  }
+
+    //Distruttori
+    ~Colors();
+
+  private:
+
+    float m_r,m_g,m_b;
+
+};
+
+#endif
