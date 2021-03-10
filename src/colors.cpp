61847bdca026878976b571fb18693790a5d99753
Elisa Legnani <elegnani97@gmail.com>
2021-03-10 14:14:04 +0100

    add operators *

--- src/colors.cpp
+++ src/colors.cpp
@@ -9,9 +9,15 @@ Color Color::operator-(Color c) {
   return Color(m_r - c.m_r, m_g - c.m_g, m_b - c.m_b);
 }
 
-bool Color::are_colors_close(Color c1, Color c2) {
-  return are_close(c1.m_r, c2.m_r) && are_close(c1.m_g, c2.m_g) &&
-         are_close(c1.m_b, c2.m_b);
+Color Color::operator*(Color c) {
+  return Color(m_r * c.m_r, m_g * c.m_g, m_b * c.m_b);
+}
+
+Color Color::operator*(float x) { return Color(m_r * x, m_g * x, m_b * x); }
+
+bool Color::is_color_close(Color c) {
+  return are_close(m_r, c.m_r) && are_close(m_g, c.m_g) &&
+         are_close(m_b, c.m_b);
 }
 
 bool Color::are_close(float x, float y) {
