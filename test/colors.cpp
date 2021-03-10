83d58a10e492e075407326ba32dfc9c1d56c44d6
Elisa Legnani <elegnani97@gmail.com>
2021-03-10 14:14:24 +0100

    changes

--- test/colors.cpp
+++ test/colors.cpp
@@ -5,7 +5,8 @@ int main() {
 
   Color c1(1.0, 2.0, 3.0);
   Color c2(5.0, 7.0, 9.0);
+  Color c3 = c1 * c2;
 
-  return c1.are_colors_close(Color{4.0, 5.0, 6.0}, c2 - c1) ? EXIT_SUCCESS
-                                                            : EXIT_FAILURE;
+  return c3.is_color_close(Color{5.0, 14.0, 27.0}) ? EXIT_SUCCESS
+                                                   : EXIT_FAILURE;
 }
