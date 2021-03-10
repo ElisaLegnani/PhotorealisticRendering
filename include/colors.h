10e5e3639b7987e4908ecc347af8a1677b6f07fe
Elisa Legnani <elegnani97@gmail.com>
2021-03-10 12:14:19 +0100

    changes

--- include/colors.h
+++ include/colors.h
@@ -1,24 +1,24 @@
-#ifndef _colors_h_
-#define _colors_h_
+#ifndef _color_h_
+#define _color_h_
 
-class Colors {
+class Color {
 
 public:
   // Costruttori
-  Colors() {
+  Color() {
     m_r = 0.0;
     m_g = 0.0;
     m_b = 0.0;
   }
 
-  Colors(float r, float g, float b) {
+  Color(float r, float g, float b) {
     m_r = r;
     m_g = g;
     m_b = b;
   }
 
   // Distruttori
-  ~Colors();
+  ~Color();
 
 private:
   float m_r, m_g, m_b;
