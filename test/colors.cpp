86a7c974295e64ee121f30bcd60042af19eb851f
Elisa Legnani <elegnani97@gmail.com>
2021-03-10 13:37:43 +0100

    changes

---
+++ test/colors.cpp
@@ -0,0 +1,13 @@
+#include "colors.h"
+#include <cstdlib>
+
+int main() {
+
+  Color c1(1.0, 2.0, 3.0);
+  Color c2(5.0, 7.0, 9.0);
+
+  // Color c3 = c3.sum_colors(c1,c2);
+
+  return c1.are_colors_close(Color{6.0, 9.0, 11.0}, c1) ? EXIT_SUCCESS
+                                                        : EXIT_FAILURE;
+}
