c5927bf6d068721b5a79dee060fda54c7cf5a4e1
Elisa Legnani <elegnani97@gmail.com>
2021-03-17 09:34:52 +0100

    changes

--- test/colors2.cpp
+++ test/colors2.cpp
@@ -7,13 +7,13 @@ int main() {
   Color c2{5.0, 7.0, 9.0};
   float x = 5.0;
 
-  if ((c1 + c2).is_color_close(Color{6.0, 9.0, 12.0}) == 0) {
+  if ((c1 * c2).is_color_close(Color{5.0, 14.0, 27.0}) == 0) {
     abort();
   }
-  if ((c2 - c1).is_color_close(Color{4.0, 5.0, 6.0}) == 0) {
+  if ((c1 * x).is_color_close(Color{5.0, 10.0, 15.0}) == 0) {
     abort();
   }
-  
+
   return 0;
 }
 
