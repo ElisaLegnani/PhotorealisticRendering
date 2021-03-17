6f946dbb5b712bb66e63cb5a2245f87404ce25f8
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-03-17 11:19:33 +0100

    costructor

--- include/hdrimage.h
+++ include/hdrimage.h
@@ -5,23 +5,29 @@ using namespace std;
 #ifndef _hdrimage_h_
 #define _hdrimage_h_
 
-class HdrImage {
+class HdrImage { //reminder: 1. width 2. height
 
 public:
   int width, height;
   vector<Color> pixels;
 
-  HdrImage(w,h){};
+  HdrImage(int w, int h){
+    width=w;
+    height=h;
+    pixels.resize(width*height);
+  };
 
   ~HdrImage(){};
   
-  bool valid_coordinates(vector<Color> p, float x, float y) {
+  bool valid_coordinates(vector<Color> p, int x, int y) {
     
   }
   
-  int pixel_offset(x,y){
+  int pixel_offset(int x, int y){
     return y * width + x;
   };
+  
+  
 
 
 };
