d92d15b4076deef2f9d61bd24fbfffb0955c393e
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-03-17 10:40:42 +0100

    Round brakets

--- test/colors.cpp
+++ test/colors.cpp
@@ -1,16 +1,17 @@
 #include "colors.h"
 #include <cstdlib>
 
+
 int main() {
 
-  Color c1{1.0, 2.0, 3.0};
-  Color c2{5.0, 7.0, 9.0};
+  Color c1(1.0, 2.0, 3.0);
+  Color c2(5.0, 7.0, 9.0);
   float x = 5.0;
 
-  if ((c1 + c2).is_color_close(Color{6.0, 9.0, 12.0}) == 0) {
+  if ((c1 + c2).is_color_close(Color(6.0, 9.0, 12.0)) == 0) {
     abort();
   }
-  if ((c2 - c1).is_color_close(Color{4.0, 5.0, 6.0}) == 0) {
+  if ((c2 - c1).is_color_close(Color(4.0, 5.0, 6.0)) == 0) {
     abort();
   }
   
--- test/colors2.cpp
+++ test/colors2.cpp
@@ -1,19 +1,20 @@
 #include "colors.h"
 #include <cstdlib>
 
+
 int main() {
 
-  Color c1{1.0, 2.0, 3.0};
-  Color c2{5.0, 7.0, 9.0};
+  Color c1(1.0, 2.0, 3.0);
+  Color c2(5.0, 7.0, 9.0);
   float x = 5.0;
 
-  if ((c1 * c2).is_color_close(Color{5.0, 14.0, 27.0}) == 0) {
+  if ((c1 * c2).is_color_close(Color(5.0, 14.0, 27.0)) == 0) {
     abort();
   }
-  if ((c1 * x).is_color_close(Color{5.0, 10.0, 15.0}) == 0) {
+  if ((c1 * x).is_color_close(Color(5.0, 10.0, 15.0)) == 0) {
     abort();
   }
-
+  
   return 0;
 }
 
