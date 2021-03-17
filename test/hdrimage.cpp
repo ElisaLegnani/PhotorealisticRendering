740b4c4b7a2181e25bbe5ff7060da55992f791a2
Elisa Legnani <elegnani97@gmail.com>
2021-03-17 11:31:37 +0100

    changes

--- test/hdrimage.cpp
+++ test/hdrimage.cpp
@@ -5,13 +5,8 @@
 
 int main() {
 
-  Color c1(1.0, 2.0, 3.0);
-  Color c2(5.0, 7.0, 9.0);
-  float x = 5.0;
-
-  if ((c1 + c2).is_color_close(Color(6.0, 9.0, 12.0)) == 0) {
-    abort();
-  }
+  HdrImage img;
+  
   if ((c2 - c1).is_color_close(Color(4.0, 5.0, 6.0)) == 0) {
     abort();
   }
