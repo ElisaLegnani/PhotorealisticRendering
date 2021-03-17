403d9f03a4b1eb478ac92ec27d3840c71050497e
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-03-17 12:02:47 +0100

    changes

--- test/hdrimage.cpp
+++ test/hdrimage.cpp
@@ -5,13 +5,9 @@
 
 int main() {
 
-  HdrImage img;
-  
-  if ((c2 - c1).is_color_close(Color(4.0, 5.0, 6.0)) == 0) {
-    abort();
-  }
-  
-  if(img.pixel_offset(0, 0) != 0 | img.pixel_offset(3, 2) != 17 | img.pixel_offset(6, 3) != (7 * 4 - 1)){
+  HdrImage img(7,4);
+
+  if(img.pixel_offset(0, 0) != 0 || img.pixel_offset(3, 2) != 17 || img.pixel_offset(6, 3) != (7 * 4 - 1)){
     abort();
   }
   
