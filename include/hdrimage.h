368b25f8fb818eff178e14abfc3df818b7265770
Elisa Legnani <elegnani97@gmail.com>
2021-03-17 10:44:24 +0100

    add method

--- include/hdrimage.h
+++ include/hdrimage.h
@@ -1,5 +1,7 @@
 #include "colors.h"
 
+using namespace std;
+
 #ifndef _hdrimage_h_
 #define _hdrimage_h_
 
@@ -7,11 +9,16 @@ class HdrImage {
 
 public:
   int width, height;
-  std::vector<Color> pixels;
+  vector<Color> pixels;
 
-  HdrImage() {}
+  HdrImage(){}
 
   ~HdrImage(){};
+
+  HdrImage valid_coordinates(vector<Color> pixels, float x, float y) {
+    return Color(m_r + c.m_r, m_g + c.m_g, m_b + c.m_b);
+  }
+
 };
 
 #endif
