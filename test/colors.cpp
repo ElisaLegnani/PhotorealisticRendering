eaed90834cd0482ec3df29945284a986b63235c6
Elisa Legnani <elegnani97@gmail.com>
2021-03-18 11:24:31 +0100

    merged tests colors

--- test/colors.cpp
+++ test/colors.cpp
@@ -14,6 +14,12 @@ int main() {
   if ((c2 - c1).is_color_close(Color(4.0, 5.0, 6.0)) == 0) {
     abort();
   }
+  if ((c1 * c2).is_color_close(Color(5.0, 14.0, 27.0)) == 0) {
+    abort();
+  }
+  if ((c1 * x).is_color_close(Color(5.0, 10.0, 15.0)) == 0) {
+    abort();
+  }
   
   return 0;
 }
