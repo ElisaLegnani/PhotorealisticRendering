57ec9ee92d4c120e77aa9ea5f95c0891a9241261
Elisa Legnani <elegnani97@gmail.com>
2021-03-18 11:50:06 +0100

    add coordinates test

--- test/hdrimage.cpp
+++ test/hdrimage.cpp
@@ -1,19 +1,24 @@
-#include "colors.h"
 #include "hdrimage.h"
+#include "colors.h"
 #include <cstdlib>
-
 
 int main() {
 
-  HdrImage img(7,4);
+  HdrImage img(7, 4);
 
-  if(img.valid_coordinates(1, 1) == 0){
+  if (img.width != 7 || img.height != 4) {
     abort();
   }
-  
-  if(img.pixel_offset(0, 0) != 0 || img.pixel_offset(3, 2) != 17 || img.pixel_offset(6, 3) != (7 * 4 - 1)){
+
+  if (img.valid_coordinates(0, 0) == 0 || img.valid_coordinates(6, 3) == 0 ||
+      img.valid_coordinates(-1, 0) != 0 || img.valid_coordinates(0, -1) != 0) {
     abort();
   }
-  
+
+  if (img.pixel_offset(0, 0) != 0 || img.pixel_offset(3, 2) != 17 ||
+      img.pixel_offset(6, 3) != (7 * 4 - 1)) {
+    abort();
+  }
+
   return 0;
 }
