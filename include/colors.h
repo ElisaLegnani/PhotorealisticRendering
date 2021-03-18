d635991832f32d678bd39a89beda1d9c3a5f1f2c
Elisa Legnani <elegnani97@gmail.com>
2021-03-18 12:11:22 +0100

    methods implemented in .h

--- include/colors.h
+++ include/colors.h
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #ifndef _color_h_
 #define _color_h_
 
@@ -18,13 +20,26 @@ public:
 
   ~Color(){};
 
-  Color operator+(Color);
-  Color operator-(Color);
-  Color operator*(Color);
-  Color operator*(float);
+  Color operator+(Color c) {
+    return Color(m_r + c.m_r, m_g + c.m_g, m_b + c.m_b);
+  }
+  Color operator-(Color c) {
+    return Color(m_r - c.m_r, m_g - c.m_g, m_b - c.m_b);
+  }
+  Color operator*(Color c) {
+    return Color(m_r * c.m_r, m_g * c.m_g, m_b * c.m_b);
+  }
+  Color operator*(float x) { return Color(m_r * x, m_g * x, m_b * x); }
 
-  bool is_color_close(Color);
-  bool are_close(float, float);
+  bool is_color_close(Color c) {
+    return are_close(m_r, c.m_r) && are_close(m_g, c.m_g) &&
+           are_close(m_b, c.m_b);
+  }
+
+  bool are_close(float x, float y) {
+    float epsilon = 1e-10;
+    return abs(x - y) < epsilon;
+  }
 
 private:
   float m_r, m_g, m_b;
