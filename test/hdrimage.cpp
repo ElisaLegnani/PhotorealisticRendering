89679830a55c39b46d616016c459bfef71a60b86
Elisa Legnani <elegnani97@gmail.com>
2021-03-18 11:31:38 +0100

    changes

--- test/hdrimage.cpp
+++ test/hdrimage.cpp
@@ -17,7 +17,3 @@ int main() {
   
   return 0;
 }
-
-//Oppure
-//return (c1 * c2).is_color_close(Color{5.0, 14.0, 27.0}) ? EXIT_SUCCESS
-//                                                        : EXIT_FAILURE;
