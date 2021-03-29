12693256b67b7535f71a0dbb131ffc84943a0330
Elisa Legnani <elegnani97@gmail.com>
2021-03-29 11:22:46 +0200

    add read_pfm

--- include/hdrimage.h
+++ include/hdrimage.h
@@ -1,5 +1,5 @@
 #include "colors.h"
-#include "write_float.h"
+#include <cstdint> // It contains uint8_t
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -11,9 +11,40 @@ using namespace std;
 #ifndef _hdrimage_h_
 #define _hdrimage_h_
 
-// Written in little endian
+enum class Endianness { little_endian, big_endian };
 
-class HdrImage { // reminder: 1. width 2. height
+void write_float(std::stringstream &stream, float value,
+                 Endianness endianness) {
+
+  // Convert "value" in a sequence of 32 bit
+  uint32_t double_word{*((uint32_t *)&value)};
+
+  // Extract the four bytes in "double_word" using bit-level operators
+  uint8_t bytes[] = {
+      static_cast<uint8_t>(double_word & 0xFF), // Least significant byte
+      static_cast<uint8_t>((double_word >> 8) & 0xFF),
+      static_cast<uint8_t>((double_word >> 16) & 0xFF),
+      static_cast<uint8_t>((double_word >> 24) & 0xFF), // Most significant byte
+  };
+
+  switch (endianness) {
+  case Endianness::little_endian:
+    for (int i{}; i < 4; ++i) // Forward loop
+      stream << bytes[i];
+    break;
+
+  case Endianness::big_endian:
+    for (int i{3}; i >= 0; --i) // Backward loop
+      stream << bytes[i];
+    break;
+  }
+}
+
+class HdrImage {  // Reminder: 1. width 2. height
+                  // Written in little endian
+
+private:
+  void read_pfm(istream &stream);
 
 public:
   int width;
@@ -27,6 +58,13 @@ public:
     width = w;
     height = h;
     pixels.resize(width * height);
+  }
+
+  HdrImage(ifstream &stream) { read_pfm(stream); }
+
+  HdrImage(const string &filename) { 
+    ifstream stream{filename};
+    read_pfm(stream);
   }
 
   ~HdrImage(){};
@@ -54,10 +92,11 @@ public:
   }
 
   void save_pfm(stringstream &sstr) { // scrivere anche save_pfm che scriva su file (ofstream)
+
     sstr << "PF\n" << width << " " << height << "\n" << endianness << "\n";
 
-    for (int y = (height - 1); y >= 0; y--) {
-      for (int x = 0; x < width; x++) {
+    for (int y{height - 1}; y >= 0; --y) {
+      for (int x{}; x < width; ++x) {
         Color color = get_pixel(x, y);
         write_float(sstr, color.r, Endianness::little_endian);
         write_float(sstr, color.g, Endianness::little_endian);
@@ -65,6 +104,8 @@ public:
       }
     }
   }
+
 };
+
 
 #endif
