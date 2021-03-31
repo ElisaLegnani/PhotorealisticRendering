88a30029f239d791281f743d9002b0c2a1634849
Elisa Legnani <elegnani97@gmail.com>
2021-03-31 13:56:19 +0200

    write main

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -1,5 +1,7 @@
 #include "colors.h"
 #include "hdrimage.h"
+#include "gd.h"     /* The library's header file */
+#include <stdio.h>
 #include <iostream>
 
 using namespace std;
@@ -10,6 +12,36 @@ int main(int argc, char *argv[]) {
     cout << "Hello, World!" << endl;
   else
     cout << "Hello, " << argv[1] << "!" << endl;
+
+
+
+
+  const int width = 256, height = 256;
+  gdImagePtr im;
+  FILE *f;
+  int row, col;
+
+  // "True color" is the old name for 24-bit RGB images
+  im = gdImageCreateTrueColor(width, height);
+
+  for(row = 0; row < height; ++row) {
+    for(col = 0; col < width; ++col) {
+      int red, green, blue = 128;
+
+      red = (int) (col * 255.0 / width);
+      green = (int) ((1.0 - row * 1.0 / height) * 255.0);
+      gdImageSetPixel(im, col, row, gdImageColorExact(im, red, green, blue));
+    }
+  }
+
+  f = fopen("image.png", "wb");
+
+  /* Output the image to the disk file in PNG format. */
+  gdImagePng(im, f);  /* gdImageJpeg(im, jpegout, -1); */
+
+  fclose(f);
+  gdImageDestroy(im);
+
 
   return 0;
 }
