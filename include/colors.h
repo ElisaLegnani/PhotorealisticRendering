16e190cbefe94ca0a2b9bb5aa5a7ced84aa1ca31
Elisa Legnani <elegnani97@gmail.com>
2021-03-31 10:33:44 +0200

    add luminosity

--- include/colors.h
+++ include/colors.h
@@ -1,4 +1,8 @@
+#include <algorithm>
 #include <cmath>
+#include <iostream>
+
+using namespace std;
 
 #ifndef _color_h_
 #define _color_h_
@@ -7,7 +11,7 @@ class Color {
 
 public:
   float r, g, b;
-  
+
   Color() {
     r = 0.0;
     g = 0.0;
@@ -22,27 +26,21 @@ public:
 
   ~Color(){};
 
-  Color operator+(Color c) {
-    return Color(r + c.r, g + c.g, b + c.b);
-  }
-  Color operator-(Color c) {
-    return Color(r - c.r, g - c.g, b - c.b);
-  }
-  Color operator*(Color c) {
-    return Color(r * c.r, g * c.g, b * c.b);
-  }
+  Color operator+(Color c) { return Color(r + c.r, g + c.g, b + c.b); }
+  Color operator-(Color c) { return Color(r - c.r, g - c.g, b - c.b); }
+  Color operator*(Color c) { return Color(r * c.r, g * c.g, b * c.b); }
   Color operator*(float x) { return Color(r * x, g * x, b * x); }
 
   bool is_color_close(Color c) {
-    return are_close(r, c.r) && are_close(g, c.g) &&
-           are_close(b, c.b);
+    return are_close(r, c.r) && are_close(g, c.g) && are_close(b, c.b);
   }
 
   bool are_close(float x, float y) {
     float epsilon = 1e-10;
     return abs(x - y) < epsilon;
   }
-  
+
+  float luminosity() { return (max(max(r, g), b) + min(min(r, g), b)) / 2.0; }
 };
 
 #endif
--- test/colors.cpp
+++ test/colors.cpp
@@ -1,7 +1,6 @@
 #include "colors.h"
 #include <cstdlib>
 
-
 int main() {
 
   Color c1(1.0, 2.0, 3.0);
@@ -20,10 +19,16 @@ int main() {
   if ((c1 * x).is_color_close(Color(5.0, 10.0, 15.0)) == 0) {
     abort();
   }
-  
+
+  // Test luminosity
+
+  if ((c1.luminosity() == 2) == 0 || (c2.luminosity() == 7) == 0) {
+    abort();
+  }
+
   return 0;
 }
 
-//Oppure
-//return (c1 * c2).is_color_close(Color{5.0, 14.0, 27.0}) ? EXIT_SUCCESS
+// Oppure
+// return (c1 * c2).is_color_close(Color{5.0, 14.0, 27.0}) ? EXIT_SUCCESS
 //                                                        : EXIT_FAILURE;
