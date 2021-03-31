205a9d15a8275fbac5453adf4b5a59ef3c6eb2cb
Elisa Legnani <elegnani97@gmail.com>
2021-03-31 14:58:25 +0200

    add read pfm from file

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -14,22 +14,31 @@ int main(int argc, char *argv[]) {
     cout << "Hello, " << argv[1] << "!" << endl;
 
 
-
-
-  const int width = 256, height = 256;
+  //const int width = 256, height = 256;
   gdImagePtr im;
   FILE *f;
   int row, col;
 
+  HdrImage img2("memorial.pfm");
+
+  const int width = img2.width, height = img2.height;  
+
   // "True color" is the old name for 24-bit RGB images
   im = gdImageCreateTrueColor(width, height);
 
+  img2.normalize_image(10000.0, 100.0);
+  //img2.clamp_image();
+
   for(row = 0; row < height; ++row) {
     for(col = 0; col < width; ++col) {
-      int red, green, blue = 128;
+      int red, green, blue;
+      Color c = img2.get_pixel(col, row);
+      red = c.r;
+      green = c.g;
+      blue = c.b;
 
-      red = (int) (col * 255.0 / width);
-      green = (int) ((1.0 - row * 1.0 / height) * 255.0);
+      //red = (int) (col * 255.0 / width);
+      //green = (int) ((1.0 - row * 1.0 / height) * 255.0);
       gdImageSetPixel(im, col, row, gdImageColorExact(im, red, green, blue));
     }
   }
