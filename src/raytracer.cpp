c81580c5a2b674948bb38ae5dcf382a55fa03ab0
Elisa Legnani <elegnani97@gmail.com>
2021-04-01 12:29:23 +0200

    fix raytracer.cpp

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -8,10 +8,6 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  if (argv[1] == NULL)
-    cout << "Hello, World!" << endl;
-  else
-    cout << "Hello, " << argv[1] << "!" << endl;
 
 
   //const int width = 256, height = 256;
