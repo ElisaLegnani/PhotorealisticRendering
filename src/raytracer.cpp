607fe4b5eff1728588308964c23a44cf3f057ee9
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-04-07 09:19:53 +0200

    fix raytracer.cpp 2

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -1,56 +1,74 @@
 #include "colors.h"
+#include "gd.h" /* The library's header file */
 #include "hdrimage.h"
-#include "gd.h"     /* The library's header file */
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  if (argv[1] == NULL)
-    cout << "Hello, World!" << endl;
-  else
-    cout << "Hello, " << argv[1] << "!" << endl;
+  string pfm_filename;
+  char* png_filename;
+  float a, gamma;
 
+  if (argv[1] == NULL) {
+    cout << "Insert input PFM filename: ";
+    cin >> pfm_filename;
+    cout << "Insert a: ";
+    cin >> a;
+    cout << "Insert gamma: ";
+    cin >> gamma;
+    cout << "Insert output PNG filename: ";
+    cin >> png_filename;
+  } else {
+    pfm_filename = argv[1];
+    a = atof(argv[2]);
+    gamma = atof(argv[3]);
+    png_filename = argv[4];
+  }
 
-  //const int width = 256, height = 256;
+  // const int width = 256, height = 256;
   gdImagePtr im;
   FILE *f;
   int row, col;
 
-  HdrImage img2("memorial.pfm");
+  HdrImage img(pfm_filename);
 
-  const int width = img2.width, height = img2.height;  
+  const int width = img.width, height = img.height;
 
   // "True color" is the old name for 24-bit RGB images
   im = gdImageCreateTrueColor(width, height);
 
-  img2.normalize_image(10000.0, 100.0);
-  //img2.clamp_image();
+  img.normalize_image(a);
+  img.clamp_image();
 
-  for(row = 0; row < height; ++row) {
-    for(col = 0; col < width; ++col) {
-      int red, green, blue;
-      Color c = img2.get_pixel(col, row);
+  for (row = 0; row < height; ++row) {
+    for (col = 0; col < width; ++col) {
+      float red, green, blue;
+      Color c = img.get_pixel(col, row);
       red = c.r;
       green = c.g;
       blue = c.b;
 
-      //red = (int) (col * 255.0 / width);
-      //green = (int) ((1.0 - row * 1.0 / height) * 255.0);
+      red = (int)(red * 255.0);
+      green = (int)(green * 255.0);
+      blue = (int)(blue * 255.0);
+      
       gdImageSetPixel(im, col, row, gdImageColorExact(im, red, green, blue));
     }
   }
 
-  f = fopen("image.png", "wb");
+  f = fopen(png_filename, "wb");
 
   /* Output the image to the disk file in PNG format. */
-  gdImagePng(im, f);  /* gdImageJpeg(im, jpegout, -1); */
+  gdImagePng(im, f); /* gdImageJpeg(im, jpegout, -1); */
 
   fclose(f);
   gdImageDestroy(im);
 
+  cout << "PNG image ready!" << endl;
 
   return 0;
 }
