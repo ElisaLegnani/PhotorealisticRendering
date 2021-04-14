884613c4a954fa6fcb0c0b2718e351c60020b40f
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-04-14 11:25:11 +0200

    Add constructors in Vec

--- include/geometry.h
+++ include/geometry.h
@@ -7,11 +7,20 @@ using namespace std;
 #ifndef _geometry_h_
 #define _geometry_h_
 
-class Vec{
+template <typename In1, typename In2, typename Out>
+Out _sum(const In1 &a, const In2 &b) {
+  return Out{a.x + b.x, a.y + b.y, a.z + b.z};
+}
 
+struct Vec{
+  float x, y, z;
+      
+      Vec(float X = 0, float Y = 0, float Z = 0) : x{X}, y{Y}, z{Z} {}
+      Vec(const Vec &); // Copy constructor
+      Vec(const Vec &&); // Move constructor
 };
 
-class Point{
+struct Point{
 
 };
 
