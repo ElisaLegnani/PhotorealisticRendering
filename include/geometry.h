4af13bfd014c26f0ce60a5339e12dae996c56c4b
Elisa Legnani <elegnani97@gmail.com>
2021-04-14 18:04:18 +0200

    Add vector operators and tests

--- include/geometry.h
+++ include/geometry.h
@@ -1,8 +1,4 @@
 #include <algorithm>
-#include <cmath>
-#include <iostream>
-
-using namespace std;
 
 #ifndef _geometry_h_
 #define _geometry_h_
@@ -12,34 +8,41 @@ Out _sum(const In1 &a, const In2 &b) {
   return Out{a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
-bool are_close(float x, float y) {
+template <typename In1, typename In2, typename Out>
+Out _diff(const In1 &a, const In2 &b) {
+  return Out{a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+bool are_xy_close(float x, float y) { //ha senso ridefinirla così?
   float epsilon = 1e-10;
   return abs(x - y) < epsilon;
 }
 
 /*bool are_xyz_close(Vec v1, Vec v2) {
-  return are_close(v1.x, v2.x) && are_close(v1.y, v2.y) && are_close(v1.z, v2.z);
+  return are_close(v1.x, v2.x) && are_close(v1.y, v2.y) && are_close(v1.z,
+v2.z);
 }*/
 
-struct Vec{
+struct Vec {
   float x, y, z;
-      
-      Vec(float X = 0, float Y = 0, float Z = 0) : x{X}, y{Y}, z{Z} {}
-      Vec(const Vec &); // Copy constructor
-      Vec(const Vec &&); // Move constructor
-  
-  Vec operator+(const Vec &a, const Vec &b) {
+
+  Vec(float X = 0, float Y = 0, float Z = 0) : x{X}, y{Y}, z{Z} {}
+  //Vec(const Vec &); // Copy constructor
+  //Vec(const Vec &&); // Move constructor
+
+  bool is_close(Vec v) {
+    return are_xy_close(x, v.x) && are_xy_close(y, v.y) && are_xy_close(z, v.z);
+  }
+};
+
+Vec operator+(const Vec &a, const Vec &b) {
     return _sum<Vec, Vec, Vec>(a, b);
   }
-  
-  bool is_close(Vec v) {
-    return are_close(x, v.x) && are_close(y, v.y) && are_close(z, v.z);
+
+Vec operator-(const Vec &a, const Vec &b) {
+    return _diff<Vec, Vec, Vec>(a, b);
   }
-  
-};
 
-struct Point{
-
-};
+struct Point {};
 
 #endif
--- test/geometry.cpp
+++ test/geometry.cpp
@@ -1,17 +1,21 @@
 #include "geometry.h"
 #include <cstdlib>
+#include <iostream>
+
+using namespace std;
 
 int main() {
 
   Vec v1(1.0, 2.0, 3.0);
   Vec v2(4.0, 6.0, 8.0);
 
-  if ((v1.is_close(v1) == 0) || (v1.is_close(v2) != 0)){
+  if ((v1.is_close(v1) == 0) || (v1.is_close(v2) != 0)) {
     cout << "Error: constructor sets wrong vector coordinates." << endl;
     abort();
   }
 
-  if(((v1 + v2).is_close(Vec(5.0, 8.0, 11.0)==0) || ((v2 - v1).is_close(Vec(3.0, 4.0, 5.0))==0) || ((v1 * 2).is_close(Vec(2.0, 4.0, 6.0)))==0){
+  if (((v1 + v2).is_close(Vec(5.0, 8.0, 11.0)) == 0) ||
+      ((v2 - v1).is_close(Vec(3.0, 4.0, 5.0)) == 0)) {
     cout << "Error: vector operators not working properly." << endl;
     abort();
   }
