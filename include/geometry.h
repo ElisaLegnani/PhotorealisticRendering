e34965d5ee7bb4a48105966e498bd51db3271690
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-04-14 10:58:57 +0200

    Create geometry library

---
+++ include/geometry.h
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+#ifndef _geometry_h_
+#define _geometry_h_
+
+class Vec{
+
+};
+
+class Point{
+
+};
+
+#endif
