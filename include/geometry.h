a6d053b54ba596486f7b7f60aa1cefe7ad034776
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-04-27 12:58:18 +0200

    Small additions

--- include/geometry.h
+++ include/geometry.h
@@ -17,20 +17,36 @@ Out _diff(const In1 &a, const In2 &b) {
   return Out{a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
-template <typename In1, typename Out> Out _prod(const In1 &a, const float &b) {
+template <typename In1, typename Out>
+Out _prod(const In1 &a, const float &b) {
   return Out{a.x * b, a.y * b, a.z * b};
 }
 
-/*bool are_xy_close(float x, float y) { //ha senso ridefinirla o usiamo
-colors.h? float epsilon = 1e-10; return abs(x - y) < epsilon;
+template <typename In, typename Out> // Da controllare
+Out _copy(const In &b) {
+  return Out{b.x, b.y, b.z};
+}
+
+/*bool are_xy_close(float x, float y) { //ha senso ridefinirla o usiamo colors.h?  // toglierei
+ float epsilon = 1e-10; return abs(x - y) < epsilon;
 }*/
 
-/*bool are_xyz_close(Vec v1, Vec v2) {
-  return are_close(v1.x, v2.x) && are_close(v1.y, v2.y) && are_close(v1.z,
-v2.z);
+/*bool are_xyz_close(Vec v1, Vec v2) { //Non lo usiamo all'interno dei metodi, ma utile per confronto Vecs in generale -> lo chiamerei are_Vec_close - oppure creiamo template
+  return are_close(v1.x, v2.x) && are_close(v1.y, v2.y) && are_close(v1.z, v2.z);
 }*/
 
-// Struct Vec
+/*template <typename In1, typename In2> // Da usare per Vec, Point, Normal
+ bool _are_xyz_close(const In1 &a, const In2 &b){
+  return are_close(a.x, b.x) && are_close(a.y, b.y) && are_close(a.z, b.z);
+ }
+ Da usare (nel main): _are_xyz_close<Vec, Vec>(v1, v2)*/
+
+/* string print_string(string type, float x, float y, float z){ //poi possiamo chiamare print all'interno.
+  return string{type+"(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"}
+ }
+ Da usare: string print(){ return print_string("Vec",x,y,z)} */
+ 
+//–––––––––––––––––––––– Struct Vec –––––––––––––––––––––––––––––––––––
 
 struct Vec {
   float x, y, z;
@@ -44,14 +60,16 @@ struct Vec {
   }
 
   string print_string() {
-    return string{"Vec(" + to_string(x) + ", " + to_string(y) + ", " +
-                  to_string(z) + ")"};
+    return string{"Vec(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"};
   }
 
+//  ||v||2
   float squared_norm() { return x*x + y*y + z*z; }
 
+//  ||v||
   float norm() { return sqrt(this->squared_norm()); }
 
+// v -> v/||v||
   void normalize() {
     float norm = this->norm();
     x /= norm;
@@ -72,17 +90,18 @@ Vec operator*(const Vec &v1, const float &c) { return _prod<Vec, Vec>(v1, c); }
 Vec operator*(const float &c, const Vec &v1) { return v1 * c; }
 
 // Scalar product
-float dot(const Vec &v1, const Vec &v2) {
-  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-}
+float dot(const Vec &v1, const Vec &v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
 
 // Vector product
 Vec cross(const Vec &v1, const Vec &v2) {
-  return Vec(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z,
-             v1.x * v2.y - v1.y * v2.x);
+  return Vec(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
 }
 
-// Struct Point
+Vec operator=(const Vec &v) {
+  return _copy<Vec, Vec>(v);
+}
+
+//–––––––––––––––––––––– Struct Point –––––––––––––––––––––––––––––––––––
 
 struct Point {
   float x, y, z;
@@ -96,8 +115,7 @@ struct Point {
   }
 
   string print_string() {
-    return string{"Point(" + to_string(x) + ", " + to_string(y) + ", " +
-                  to_string(z) + ")"};
+    return string{"Point(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"};
   }
 };
 
@@ -118,12 +136,32 @@ Vec operator-(const Point &p1, const Point &p2) {
   return _diff<Point, Point, Vec>(p1, p2);
 }
 
-// Struct Normal
+Point operator=(const Point &p) {
+  return _copy<Point, Point>(p);
+}
+
+//–––––––––––––––––––––– Struct Normal –––––––––––––––––––––––––––––––––––
 
 struct Normal {
   float x, y, z;
 
   Normal(float X = 0, float Y = 0, float Z = 0) : x{X}, y{Y}, z{Z} {}
+  
+  bool is_close(Normal n) {
+    return are_close(x, n.x) && are_close(y, n.y) && are_close(z, n.z);
+  }
+
+  string print_string() {
+    return string{"Normal(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"};
+  }
+  
+//  Da implementare anche le altre?
 };
+
+//–––––––––––––––––––––– Unit vectors i, j, k –––––––––––––––––––––––––––––––––––
+
+Vec VEC_X(1.0, 0.0, 0.0);
+Vec VEC_Y(0.0, 1.0, 0.0);
+Vec VEC_Z(0.0, 0.0, 1.0);
 
 #endif
