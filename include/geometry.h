153c281776080b0b16e687dbd20e9abb6b92f179
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-04-27 13:28:30 +0200

    Implement copy and move constructors (to check)

--- include/geometry.h
+++ include/geometry.h
@@ -52,9 +52,11 @@ struct Vec {
   float x, y, z;
 
   Vec(float X = 0, float Y = 0, float Z = 0) : x{X}, y{Y}, z{Z} {}
-  // Vec(const Vec &); // Copy constructor
-  // Vec(const Vec &&); // Move constructor
-
+  
+  Vec(const Vec &vc){ x=vc.x; y=vc.y; z=vc.z; } // Copy constructor da implementare in .cpp? // Vec(const Vec &); piu elegante, ma da verificare: Vec(const Vec &vc):Vec(*vc.x,*vc.y,*vc.z) {}
+  Vec(const Vec &&vm) : x{vm.x}, y{vm.y}, z{vm.z} {} // Move constructor // Vec(const Vec &&);
+//  Move constructor: to create temporary objects for quick operations (no memory needed)
+  
   bool is_close(Vec v) {
     return are_close(x, v.x) && are_close(y, v.y) && are_close(z, v.z);
   }
