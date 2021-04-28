b77c73f7ae38494e588f9896ccccffd57cadcabe
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-04-28 11:35:43 +0200

    Fix bugs copy & move constructors

--- include/geometry.h
+++ include/geometry.h
@@ -3,8 +3,6 @@
 #include <iostream>
 #include <sstream>
 
-// CREARE GEOMETRY.CPP !!!
-
 using namespace std;
 
 #ifndef _geometry_h_
@@ -30,15 +28,13 @@ Out _copy(const In &b) {
   return Out{b.x, b.y, b.z};
 }*/
 
-//da chiamare _are_close, più coerente
-template <typename In> bool are_xyz_close(const In &a, const In &b){ //meglio con template! non ci sono problemi con copy e move constructors
+template <typename In> bool are_xyz_close(const In &a, const In &b){
   return are_close(a.x, b.x) && are_close(a.y, b.y) && are_close(a.z, b.z);
 }
 
-/* string print_string(string type, float x, float y, float z){ //poi possiamo chiamare print all'interno.
-  return string{type+"(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"}
+string xyz_string(string type, float x, float y, float z){
+  return string{type+"(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"};
  }
- Da usare: string print(){ return print_string("Vec",x,y,z)} */
 
  
 //–––––––––––––––––––––– Struct Vec –––––––––––––––––––––––––––––––––––
@@ -48,21 +44,18 @@ struct Vec {
 
   Vec(float X = 0, float Y = 0, float Z = 0) : x{X}, y{Y}, z{Z} {}
   
-  Vec(const Vec &); // Copy constructor
-  Vec(const Vec &&); // Move constructor
+// Vec(const Vec &); // Copy constructor
+// Vec(const Vec &&); // Move constructor
 
-  //Vec(const Vec &vc){ x=vc.x; y=vc.y; z=vc.z; } // Copy constructor da implementare in .cpp? // Vec(const Vec &); piu elegante, ma da verificare: Vec(const Vec &vc):Vec(*vc.x,*vc.y,*vc.z) {}
-  //Vec(const Vec &&vm) : x{vm.x}, y{vm.y}, z{vm.z} {} // Move constructor // Vec(const Vec &&);
+//  Vec(const Vec &vc){ x=vc.x; y=vc.y; z=vc.z; } // Copy constructor da implementare in .cpp? // Vec(const Vec &); piu elegante, ma da verificare: Vec(const Vec &vc):Vec(*vc.x,*vc.y,*vc.z) {}
+//  Vec(const Vec &&vm) : x{vm.x}, y{vm.y}, z{vm.z} {} // Move constructor // Vec(const Vec &&);
   //Move constructor: to create temporary objects for quick operations (no memory needed)
   
-  // meglio con template
-  /*bool is_close(Vec v) {
+  bool is_close(Vec v) {
     return are_close(x, v.x) && are_close(y, v.y) && are_close(z, v.z);
-  }*/ 
-
-  string print_string() {
-    return string{"Vec(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"};
   }
+
+  string get_string(){ return xyz_string("Vec",x,y,z);}
 
 //  ||v||2
   float squared_norm() { return x*x + y*y + z*z; }
@@ -108,17 +101,16 @@ struct Point {
   float x, y, z;
 
   Point(float X = 0, float Y = 0, float Z = 0) : x{X}, y{Y}, z{Z} {}
-  Point(const Point &); // Copy constructor
-  Point(const Point &&); // Move constructor
-
-  // meglio con template
-  /*bool is_close(Point p) {
+  
+  Point(const Point &vc){ x=vc.x; y=vc.y; z=vc.z; } // Copy constructor: piu elegante?
+  Point(const Point &&vm) : x{vm.x}, y{vm.y}, z{vm.z} {} // Move constructor
+  
+bool is_close(Point p) {
     return are_close(x, p.x) && are_close(y, p.y) && are_close(z, p.z);
-  }*/
-
-  string print_string() {
-    return string{"Point(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"};
   }
+
+  string get_string(){ return xyz_string("Point",x,y,z);}
+  
 };
 
 Point operator+(const Point &p, const Vec &v) {
@@ -151,14 +143,11 @@ struct Normal {
   Normal(const Normal &); // Copy constructor
   Normal(const Normal &&); // Move constructor
 
-  // meglio con template
-  /*bool is_close(Normal n) {
+  bool is_close(Normal n) {
     return are_close(x, n.x) && are_close(y, n.y) && are_close(z, n.z);
-  }*/
-
-  string print_string() {
-    return string{"Normal(" + to_string(x) + ", " + to_string(y) + ", " + to_string(z) + ")"};
   }
+
+  string get_string() { return xyz_string("Normal",x,y,z);}
   
 //  Da implementare anche le altre? - non credo
 };
@@ -168,92 +157,5 @@ struct Normal {
 Vec VEC_X(1.0, 0.0, 0.0);
 Vec VEC_Y(0.0, 1.0, 0.0);
 Vec VEC_Z(0.0, 0.0, 1.0);
-
-// servono?
-
-//–––––––––––––––––––––– Struct Transformation –––––––––––––––––––––––––––––––––––
-
-float IDENTITY_MATR4x4[4][4] = {{1.0, 0.0, 0.0, 0.0},
-                                {0.0, 1.0, 0.0, 0.0},
-                                {0.0, 0.0, 1.0, 0.0},
-                                {0.0, 0.0, 0.0, 1.0}};
-
-bool are_matr_close(float m1[4][4], float m2[4][4]){
-  for (int i{}; i<4; ++i){
-    for (int j{}; j<4; ++j){
-      if(!(are_close(m1[i][j], m2[i][j]))) return false;
-    }
-  }
-  return true;
-}
-
-void matr_prod(const float m1[4][4], const float m2[4][4], float m[4][4]){
-  for (int i{}; i<4; ++i){
-    for (int j{}; j<4; ++j){
-      for (int k{}; k<4; ++k){
-        m[i][j] += m1[i][k] * m[k][j];
-      }  
-    }
-  }
-}
-
-struct Transformation {
-
-  //float m[4][4] = IDENTITY_MATR4x4;  //error: array must be initialized with a brace-enclosed initializer
-  //float invm[4][4] = IDENTITY_MATR4x4;
-
-  float m[4][4] = {{1.0, 0.0, 0.0, 0.0},
-                   {0.0, 1.0, 0.0, 0.0},
-                   {0.0, 0.0, 1.0, 0.0},
-                   {0.0, 0.0, 0.0, 1.0}};
-
-  float invm[4][4] = {{1.0, 0.0, 0.0, 0.0},
-                      {0.0, 1.0, 0.0, 0.0},
-                      {0.0, 0.0, 1.0, 0.0},
-                      {0.0, 0.0, 0.0, 1.0}};
-
-  Transformation(){};
-
-  Transformation(float M[4][4], float invM[4][4]){
-    for (int i{}; i<4; ++i){
-      for (int j{}; j<4; ++j){
-        m[i][j] = M[i][j];
-        invm[i][j] = invM[i][j];
-      }
-    }
-  };
-  
-  string print_string() {
-    ostringstream stream;
-    stream << "Transformation \n";
-    for (int i{}; i<4; ++i){
-      for (int j{}; j<4; ++j){
-        stream << m[i][j] << " ";
-      }
-      stream << "\n";
-    }
-    return stream.str();
-  }
-
-  bool is_close(Transformation t){
-    return are_matr_close(m, t.m) && are_matr_close(invm, t.invm);
-  }
-
-  bool is_consistent(){
-    float prod[4][4] = {};
-    matr_prod(m, invm, prod);
-    return are_matr_close(prod, IDENTITY_MATR4x4);
-  }
-
-  Transformation inverse(){
-    return Transformation{invm, m};
-  }
-
-};
-
-Transformation translation(Vec); //implementare
-
-//implementare tutte le altre trasformazioni!
-
 
 #endif
--- test/geometry.cpp
+++ test/geometry.cpp
@@ -8,26 +8,60 @@ int main() {
 
   Vec v1(1.0, 2.0, 3.0);
   Vec v2(4.0, 6.0, 8.0);
-
-  // Test riscritti sotto con are_xyz_close
-  /*if ((v1.is_close(v1) == 0) || (v1.is_close(v2) != 0)) {
+  
+  cout << v1.get_string()<< endl;
+  cout << v2.get_string() << endl;
+  Vec v3(v1);
+  
+  cout << v3.get_string()<< endl;
+ 
+  if (v1.is_close(v1) == 0 || v1.is_close(v2) != 0) {
     cout << "Error: constructor sets wrong vector coordinates." << endl;
     abort();
   }
 
-  if (//((-v1).is_close(Vec(-1.0, -2.0, -3.0)) == 0) ||
-      ((v1 + v2).is_close(Vec(5.0, 8.0, 11.0)) == 0) ||
-      ((v2 - v1).is_close(Vec(3.0, 4.0, 5.0)) == 0) ||
-      ((2 * v1).is_close(Vec(2.0, 4.0, 6.0)) == 0) ||
-      ((v1 * 2).is_close(Vec(2.0, 4.0, 6.0)) == 0) ||
-      ((are_close(dot(v1, v2), 40.0)) == 0) ||
-      (((cross(v1, v2)).is_close(Vec(-2.0, 4.0, -2.0))) == 0) ||
-      (((cross(v2, v1)).is_close(Vec(2.0, -4.0, 2.0))) == 0) ||
-      ((are_close(v1.squared_norm(), 14.0)) == 0) ||
-      ((are_close(pow(v1.norm(),2), 14.0)) == 0)) {
+  if (//(-v1).is_close(Vec(-1.0, -2.0, -3.0) == 0) ||
+      (v1 + v2).is_close(Vec(5.0, 8.0, 11.0)) == 0 ||
+      (v2 - v1).is_close(Vec(3.0, 4.0, 5.0)) == 0 ||
+      (2 * v1).is_close(Vec(2.0, 4.0, 6.0)) == 0 ||
+      (v1 * 2).is_close(Vec(2.0, 4.0, 6.0)) == 0 ||
+      are_close(dot(v1, v2), 40.0) == 0 ||
+      (cross(v1, v2)).is_close(Vec(-2.0, 4.0, -2.0)) == 0 ||
+      (cross(v2, v1)).is_close(Vec(2.0, -4.0, 2.0)) == 0 ||
+      are_close(v1.squared_norm(), 14.0) == 0 //||
+      //((are_close(pow(v1.norm(),2), 14.0)) == 0)
+      ){
     cout << "Error: vector operators not working properly." << endl;
     abort();
-  }*/
+  }
+
+
+
+  // Test Point
+
+  Point p1(1.0, 2.0, 3.0);
+  Point p2(4.0, 6.0, 8.0);
+
+  cout << p1.get_string() << endl;
+  cout << p2.get_string() << endl;
+  
+/*
+  if (p1.is_close(p1) == 0 || p1.is_close(p2) != 0) {
+    cout << "Error: constructor sets wrong point coordinates." << endl;
+    abort();
+  }
+
+  if ((p1 * 2).is_close(Point(2.0, 4.0, 6.0)) == 0 ||
+      (p1 + v2).is_close(Point(5.0, 8.0, 11.0)) == 0 ||
+      (p2 - p1).is_close(Vec(3.0, 4.0, 5.0)) == 0 ||
+      (p1 - v2).is_close(Point(-3.0, -4.0, -5.0)) == 0) {
+    cout << "Error: point operators not working properly." << endl;
+    abort();
+  }
+*/
+  
+
+//  Test are_xyz_close (as template)
 
   if (are_xyz_close(v1, v1) == 0 || are_xyz_close(v1, v2) != 0) {
     cout << "Error: constructor sets wrong vector coordinates." << endl;
@@ -48,28 +82,7 @@ int main() {
     cout << "Error: vector operators not working properly." << endl;
     abort();
   }
-
-
-
-  // Test Point
-
-  Point p1(1.0, 2.0, 3.0);
-  Point p2(4.0, 6.0, 8.0);
-
-  // Test riscritti sotto con are_xyz_close
-  /*if ((p1.is_close(p1) == 0) || (p1.is_close(p2) != 0)) {
-    cout << "Error: constructor sets wrong point coordinates." << endl;
-    abort();
-  }
-
-  if (((p1 * 2).is_close(Point(2.0, 4.0, 6.0)) == 0) ||
-      ((p1 + v2).is_close(Point(5.0, 8.0, 11.0)) == 0) ||
-      ((p2 - p1).is_close(Vec(3.0, 4.0, 5.0)) == 0) ||
-      ((p1 - v2).is_close(Point(-3.0, -4.0, -5.0)) == 0)) {
-    cout << "Error: point operators not working properly." << endl;
-    abort();
-  }*/
-
+  
   if (are_xyz_close(p1, p1) == 0 || are_xyz_close(p1, p2) != 0) {
     cout << "Error: constructor sets wrong point coordinates." << endl;
     abort();
@@ -82,11 +95,6 @@ int main() {
     cout << "Error: point operators not working properly." << endl;
     abort();
   }
-
-  // Test Transformation
-
-  Transformation t1;
-  cout << t1.print_string();
-
+  
   return 0;
 }
