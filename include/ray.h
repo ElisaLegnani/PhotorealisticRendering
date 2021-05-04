c5dd31da2b746e5749b56c34304a936571f4e0e1
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-05-04 13:56:34 +0200

    Set up Ray struct with tests

--- include/ray.h
+++ include/ray.h
@@ -1,1 +1,31 @@
+//#include "colors.h"
+#include "geometry.h"
+#include <cmath>
+//#include <iostream>
+//#include <sstream>
 
+using namespace std;
+
+#ifndef _ray_h_
+#define _ray_h_
+
+struct Ray {
+  Point origin;
+  Vec dir;
+  float tmin=1e-5;
+  float tmax=INFINITY;
+  int depth=0;
+  
+  Ray(Point o, Vec d){
+    origin(o);
+    dir(d);
+  }
+  
+  bool is_close(Ray r) {
+    return origin.is_close(r.origin) && dir.is_close(r.dir);
+  }
+  
+  Point at(float factor){ return origin + dir * factor; }
+  
+};
+#endif
---
+++ test/ray.cpp
@@ -0,0 +1,70 @@
+#include "transformation.h"
+#include "ray.h"
+#include <algorithm>
+#include <cstdlib>
+
+//–––––––––– Functions supporting tests –––––––––––––––
+
+void test_consistency(Transformation t, string s="is_consistent()"){
+  if (t.is_consistent() == 0) {
+    cout << "Error: "<< s <<"." << endl;
+    abort();
+  }
+}
+
+void test_closeness(Ray r1, Ray r2, string s="is_close()"){
+  if (r1.is_close(r2) == 0) {
+    cout << "Error: "<< s <<"." << endl;
+    cout << "Origin1: "; r1.origin.get_string();
+    cout << "Dir1:"; r1.dir.get_string();
+    abort();
+  }
+}
+
+void test_not_closeness(Ray r1, Ray r2, string s="is_close()"){
+  if (r1.is_close(r2) != 0) {
+    cout << "Error: "<<s<<"." << endl;
+    cout << "Origin1: "; r1.origin.get_string();
+    cout << "Dir1:"; r1.dir.get_string();
+    abort();
+  }
+}
+
+void test_closeness(Point p1, Point p2, string s="is_close()"){
+  if (p1.is_close(p2) == 0) {
+    cout << "Error: "<< s <<"." << endl;
+    abort();
+  }
+}
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+int main() {
+  
+  Ray ray1(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
+  Ray ray2(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
+  Ray ray3(Point(5.0, 1.0, 4.0), Vec(3.0, 9.0, 4.0));
+
+  cout << "Origin1: "; ray1.origin.get_string();
+  cout << "Dir1:"; ray1.dir.get_string();
+  test_closeness(ray1, ray2);
+  test_not_closeness(ray1,ray3);
+  
+//  Test at
+
+  Ray ray4(Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0));
+  test_closeness(ray4.at(0.0),ray4.origin, "at()");
+  test_closeness(ray4.at(1.0),Point(5.0, 4.0, 5.0), "at()");
+  test_closeness(ray4.at(2.0),Point(9.0, 6.0, 6.0), "at()");
+
+  //  Test transform
+  
+  /*
+  Ray ray5(Point(1.0, 2.0, 3.0), Vec(6.0, 5.0, 4.0));
+  Transformation tr=translation(Vec(10.0, 11.0, 12.0)) * rotation_x(90.0));
+ Ray transformed=
+          transformed = ray.transform(transformation)
+          assert transformed.origin.is_close(Point(11.0, 8.0, 14.0))
+          assert transformed.dir.is_close(Vec(6.0, -4.0, 5.0))*/
+
+return 0;
+}
