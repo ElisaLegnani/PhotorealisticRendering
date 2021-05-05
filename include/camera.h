61edcc44cec4bb6c0bd2ece7a34a73a1bb504e54
Elisa Legnani <elegnani97@gmail.com>
2021-05-05 14:17:58 +0200

    Add PerspectiveCamera struct and tests

--- include/camera.h
+++ include/camera.h
@@ -21,4 +21,23 @@ struct OrthogonalCamera : public Camera {
   }
 };
 
+struct PerspectiveCamera : public Camera {
+  Transformation transformation;
+  float aspect_ratio;
+  float screen_dist;
+
+  PerspectiveCamera(float d = 1.0, float a = 1.0, Transformation t = Transformation()) : screen_dist{d}, aspect_ratio{a}, transformation{t} {}
+  
+  Ray fire_ray(float u, float v){
+    Point o = Point(-screen_dist, 0.0, 0.0);
+    Vec d = Vec(screen_dist, (1.0-2.0*u)*aspect_ratio, 2.0*v-1.0);
+    return Ray(o, d, 1.0, INFINITY, 0).transform(transformation);
+  }
+
+  float aperture_deg(){
+    return 2.0 * atan(screen_dist/aspect_ratio) * 180.0 / M_PI;
+  }
+};
+
+
 #endif
