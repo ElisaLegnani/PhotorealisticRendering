b32ada329cccf588805bf22ac5b34285381f4f00
Elisa Legnani <elegnani97@gmail.com>
2021-05-13 19:42:38 +0200

    Add Catch2 testing to cameratest

--- test/camera.cpp
+++ test/camera.cpp
@@ -1,80 +1,75 @@
 #include "camera.h"
+#include "catch_amalgamated.hpp"
 
-//–––––––––– Functions supporting tests –––––––––––––––
+#define CATCH_CONFIG_MAIN
 
-void test_closeness(float a, float b, string s="is_close()"){
-  if (are_close(a, b) == 0) {
-    cout << "Error: "<< s <<"." << endl;
-    abort();
-  }
+// ––––––––––––––––– Test OrthogonalCamera –––––––––––––––––
+
+OrthogonalCamera cam(2.0);
+
+// Fire one ray for each corner of the image plane
+Ray ray1 = cam.fire_ray(0.0, 0.0);
+Ray ray2 = cam.fire_ray(1.0, 0.0);
+Ray ray3 = cam.fire_ray(0.0, 1.0);
+Ray ray4 = cam.fire_ray(1.0, 1.0);
+
+// Verify that the rays are parallel by verifying that cross-products vanish
+TEST_CASE("OrthogonalCamera: rays not parallel", "[orthogonal camera]") {
+  REQUIRE(are_close(0.0, cross(ray1.dir, ray2.dir).squared_norm()));
+  REQUIRE(are_close(0.0, cross(ray1.dir, ray3.dir).squared_norm()));
+  REQUIRE(are_close(0.0, cross(ray1.dir, ray4.dir).squared_norm()));
 }
 
-void test_closeness(Point p1, Point p2, string s="is_close()"){
-  if (p1.is_close(p2) == 0) {
-    cout << "Error: "<< s <<"." << endl;
-    abort();
-  }
+// Verify that the ray hitting the corners have the right coordinates
+TEST_CASE("OrthogonalCamera: wrong hitting coordinates",
+          "[orthogonal camera]") {
+  REQUIRE(ray1.at(1.0).is_close(Point(0.0, 2.0, -1.0)));
+  REQUIRE(ray2.at(1.0).is_close(Point(0.0, -2.0, -1.0)));
+  REQUIRE(ray3.at(1.0).is_close(Point(0.0, 2.0, 1.0)));
+  REQUIRE(ray4.at(1.0).is_close(Point(0.0, -2.0, 1.0)));
 }
 
+// Test transform
+Transformation tr = translation(-VEC_Y * 2.0) * rotation_z(90);
+OrthogonalCamera cam2(1.0, tr);
+Ray ray = cam2.fire_ray(0.5, 0.5);
 
-int main() {
+TEST_CASE("OrthogonalCamera: transform", "[orthogonal camera]") {
+  REQUIRE(ray.at(1.0).is_close(Point(0.0, -2.0, 0.0)));
+}
 
-  //–––––––– Test OrthogonalCamera ––––––––
+// –––––––––––––––––  Test PerspectiveCamera –––––––––––––––––
 
-  OrthogonalCamera cam(2.0);
+PerspectiveCamera cam3(1.0, 2.0);
 
-  // Fire one ray for each corner of the image plane
-  Ray ray1 = cam.fire_ray(0.0, 0.0);
-  Ray ray2 = cam.fire_ray(1.0, 0.0);
-  Ray ray3 = cam.fire_ray(0.0, 1.0);
-  Ray ray4 = cam.fire_ray(1.0, 1.0);
+// Fire one ray for each corner of the image plane
+Ray ray5 = cam3.fire_ray(0.0, 0.0);
+Ray ray6 = cam3.fire_ray(1.0, 0.0);
+Ray ray7 = cam3.fire_ray(0.0, 1.0);
+Ray ray8 = cam3.fire_ray(1.0, 1.0);
 
-  // Verify that the rays are parallel by verifying that cross-products vanish
-  test_closeness(0.0, cross(ray1.dir, ray2.dir).squared_norm(), "OrthogonalCamera, rays not parallel");
-  test_closeness(0.0, cross(ray1.dir, ray3.dir).squared_norm(), "OrthogonalCamera, rays not parallel");
-  test_closeness(0.0, cross(ray1.dir, ray4.dir).squared_norm(), "OrthogonalCamera, rays not parallel");
+// Verify that all the rays depart from the same point
+TEST_CASE("PerspectiveCamera: rays do not depart from the same point",
+          "[perspective camera]") {
+  REQUIRE(ray5.origin.is_close(ray6.origin));
+  REQUIRE(ray5.origin.is_close(ray7.origin));
+  REQUIRE(ray5.origin.is_close(ray8.origin));
+}
 
-  // Verify that the ray hitting the corners have the right coordinates
-  test_closeness(ray1.at(1.0), Point(0.0, 2.0, -1.0), "OrthogonalCamera, wrong hitting coordinates");
-  test_closeness(ray2.at(1.0), Point(0.0, -2.0, -1.0), "OrthogonalCamera, wrong hitting coordinates");
-  test_closeness(ray3.at(1.0), Point(0.0, 2.0, 1.0), "OrthogonalCamera, wrong hitting coordinates");
-  test_closeness(ray4.at(1.0), Point(0.0, -2.0, 1.0), "OrthogonalCamera, wrong hitting coordinates");
+// Verify that the ray hitting the corners have the right coordinates
+TEST_CASE("PerspectiveCamera: wrong hitting coordinates",
+          "[perspective camera]") {
+  REQUIRE(ray5.at(1.0).is_close(Point(0.0, 2.0, -1.0)));
+  REQUIRE(ray6.at(1.0).is_close(Point(0.0, -2.0, -1.0)));
+  REQUIRE(ray7.at(1.0).is_close(Point(0.0, 2.0, 1.0)));
+  REQUIRE(ray8.at(1.0).is_close(Point(0.0, -2.0, 1.0)));
+}
 
+// Test transform
+Transformation tr2 = translation(-VEC_Y * 2.0) * rotation_z(90);
+PerspectiveCamera cam4(1.0, 1.0, tr2);
+Ray ray9 = cam4.fire_ray(0.5, 0.5);
 
-  //Test transform
-  Transformation tr = translation(-1*VEC_Y * 2.0) * rotation_z(M_PI/2.0); //risolvere -vec & rotation(rad-deg) !
-  OrthogonalCamera cam2(1.0, tr);
-  Ray ray = cam2.fire_ray(0.5, 0.5);
-  test_closeness(ray.at(1.0), Point(0.0, -2.0, 0.0), "OrthogonalCamera, transform");
-
-
-  //–––––––– Test PerspectiveCamera –––––––– 
-
-  PerspectiveCamera cam3(1.0, 2.0);
-
-  // Fire one ray for each corner of the image plane
-  Ray ray5 = cam3.fire_ray(0.0, 0.0);
-  Ray ray6 = cam3.fire_ray(1.0, 0.0);
-  Ray ray7 = cam3.fire_ray(0.0, 1.0);
-  Ray ray8 = cam3.fire_ray(1.0, 1.0);
-
-  // Verify that all the rays depart from the same point
-  test_closeness(ray5.origin, ray6.origin, "PerspectiveCamera, rays do not depart from the same point");
-  test_closeness(ray5.origin, ray7.origin, "PerspectiveCamera, rays do not depart from the same point");
-  test_closeness(ray5.origin, ray8.origin, "PerspectiveCamera, rays do not depart from the same point");
-
-  // Verify that the ray hitting the corners have the right coordinates
-  test_closeness(ray5.at(1.0), Point(0.0, 2.0, -1.0), "PerspectiveCamera, wrong hitting coordinates");
-  test_closeness(ray6.at(1.0), Point(0.0, -2.0, -1.0), "PerspectiveCamera, wrong hitting coordinates");
-  test_closeness(ray7.at(1.0), Point(0.0, 2.0, 1.0), "PerspectiveCamera, wrong hitting coordinates");
-  test_closeness(ray8.at(1.0), Point(0.0, -2.0, 1.0), "PerspectiveCamera, wrong hitting coordinates");
-
-
-  // Test transform
-  Transformation tr2 = translation(-1*VEC_Y * 2.0) * rotation_z(M_PI/2.0); //risolvere -vec & rotation(rad-deg) !
-  OrthogonalCamera cam4(1.0, tr2);
-  Ray ray9 = cam4.fire_ray(0.5, 0.5);
-  test_closeness(ray9.at(1.0), Point(0.0, -2.0, 0.0), "OrthogonalCamera, transform");
-  
-  return 0;
+TEST_CASE("PerspectiveCamera: transform", "[perspective camera]") {
+  REQUIRE(ray9.at(1.0).is_close(Point(0.0, -2.0, 0.0)));
 }
