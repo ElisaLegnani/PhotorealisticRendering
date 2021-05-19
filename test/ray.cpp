d58e313fda2b737d36c55c9993d841048a443b96
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-05-19 10:53:46 +0200

    Fix issue #7 (#8)
    * Add Catch2 testing to all tests
    
    * Add amalgamated header and source files for Catch2
    
    * Update CMakeLists with C++17
    
    * Changed is_color_close to is_close (handy for tests)
    
    Co-authored-by: Elisa Legnani <elegnani97@gmail.com>
    Co-authored-by: ElisaLegnani <62106779+ElisaLegnani@users.noreply.github.com>

--- test/ray.cpp
+++ test/ray.cpp
@@ -1,67 +1,31 @@
-#include "transformation.h"
 #include "ray.h"
+#include "catch_amalgamated.hpp"
+#include "transformation.h"
 
-//–––––––––– Functions supporting tests –––––––––––––––
+#define CATCH_CONFIG_MAIN
 
-void test_closeness(Ray r1, Ray r2, string s="is_close()"){
-  if (r1.is_close(r2) == 0) {
-    cout << "Error: "<< s <<"." << endl;
-    cout << "Origin1: "; r1.origin.get_string();
-    cout << "Dir1:"; r1.dir.get_string();
-    abort();
-  }
+Ray ray1(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
+Ray ray2(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
+Ray ray3(Point(5.0, 1.0, 4.0), Vec(3.0, 9.0, 4.0));
+
+TEST_CASE("Ray constructor", "[ray]") {
+  REQUIRE(ray1.is_close(ray2));
+  REQUIRE(!ray1.is_close(ray3));
 }
 
-void test_not_closeness(Ray r1, Ray r2, string s="is_close()"){
-  if (r1.is_close(r2) != 0) {
-    cout << "Error: "<<s<<"." << endl;
-    cout << "Origin1: "; r1.origin.get_string();
-    cout << "Dir1:"; r1.dir.get_string();
-    abort();
-  }
+Ray ray4(Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0));
+
+TEST_CASE("Ray at", "[ray]") {
+  REQUIRE(ray4.at(0.0).is_close(ray4.origin));
+  REQUIRE(ray4.at(1.0).is_close(Point(5.0, 4.0, 5.0)));
+  REQUIRE(ray4.at(2.0).is_close(Point(9.0, 6.0, 6.0)));
 }
 
-void test_closeness(Point p1, Point p2, string s="is_close()"){
-  if (p1.is_close(p2) == 0) {
-    cout << "Error: "<< s <<"." << endl;
-    abort();
-  }
-}
+Ray ray5(Point(1.0, 2.0, 3.0), Vec(6.0, 5.0, 4.0));
+Transformation tr = translation(Vec(10.0, 11.0, 12.0)) * rotation_x(90.0);
+Ray transformed = ray5.transform(tr);
 
-void test_closeness(Vec v1, Vec v2, string s="is_close()"){
-  if (v1.is_close(v2) == 0) {
-    cout << "Error: "<< s <<"." << endl;
-    abort();
-  }
-}
-//––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
-int main() {
-  
-  Ray ray1(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
-  Ray ray2(Point(1.0, 2.0, 3.0), Vec(5.0, 4.0, -1.0));
-  Ray ray3(Point(5.0, 1.0, 4.0), Vec(3.0, 9.0, 4.0));
-
-  test_closeness(ray1, ray2);
-  test_not_closeness(ray1,ray3);
-  
-//  Test at
-
-  Ray ray4(Point(1.0, 2.0, 4.0), Vec(4.0, 2.0, 1.0));
-  test_closeness(ray4.at(0.0),ray4.origin, "at()");
-  test_closeness(ray4.at(1.0),Point(5.0, 4.0, 5.0), "at()");
-  test_closeness(ray4.at(2.0),Point(9.0, 6.0, 6.0), "at()");
-
-  //  Test transform
-  
-  Ray ray5(Point(1.0, 2.0, 3.0), Vec(6.0, 5.0, 4.0));
-  
-  Transformation tr=translation(Vec(10.0, 11.0, 12.0))* rotation_x(90.0);
-  
-  Ray transformed=ray5.transform(tr);
-  
-  test_closeness(transformed.origin,Point(11.0, 8.0, 14.0), "transform() on origin");
-  test_closeness(transformed.dir,Vec(6.0, -4.0, 5.0), "transform() on direction");
-
-return 0;
+TEST_CASE("Ray transform", "[ray]") {
+  REQUIRE(transformed.origin.is_close(Point(11.0, 8.0, 14.0)));
+  REQUIRE(transformed.dir.is_close(Vec(6.0, -4.0, 5.0)));
 }
--- test/transformation.cpp
+++ test/transformation.cpp
@@ -1,183 +1,151 @@
 #include "transformation.h"
+#include "catch_amalgamated.hpp"
 
-//–––––––––– Functions supporting tests –––––––––––––––
+#define CATCH_CONFIG_MAIN
 
-void test_consistency(Transformation t, string s="is_consistent()"){
-  if (t.is_consistent() == 0) {
-    cout << "Error: "<< s <<"." << endl;
-    abort();
-  }
+// test get_string da fare ?
+
+float m[4][4] = {{1.0, 2.0, 3.0, 4.0},
+                 {5.0, 6.0, 7.0, 8.0},
+                 {9.0, 9.0, 8.0, 7.0},
+                 {6.0, 5.0, 4.0, 1.0}};
+
+float invm[4][4] = {{-3.75, 2.75, -1, 0},
+                    {4.375, -3.875, 2.0, -0.5},
+                    {0.5, 0.5, -1.0, 1.0},
+                    {-1.375, 0.875, 0.0, -0.5}};
+
+Transformation t(m, invm);
+
+TEST_CASE("Transformation consistency", "[transformation]") {
+  REQUIRE(t.is_consistent());
 }
 
-void test_closeness(Transformation t1, Transformation t2, string s="is_close()"){
-  if (t1.is_close(t2) == 0) {
-    cout << "Error: "<< s <<"." << endl;
-    abort();
-  }
-}
+TEST_CASE("Transformation is_close", "[transformation]") {
 
-void test_not_closeness(Transformation t1, Transformation t2, string s="is_close()"){
-  if (t1.is_close(t2) != 0) {
-    cout << "Error: "<<s<<"." << endl;
-    abort();
-  }
-}
-//––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
-int main() {
-
-/*test get_string da fare
-  Transformation t1;
-  cout << t1.get_string() <<endl;
-*/
-
-  float m[4][4] = {{1.0, 2.0, 3.0, 4.0},
-                    {5.0, 6.0, 7.0, 8.0},
-                    {9.0, 9.0, 8.0, 7.0},
-                    {6.0, 5.0, 4.0, 1.0}};
-  
-  float invm[4][4] = {{-3.75, 2.75, -1, 0},
-                      {4.375, -3.875, 2.0, -0.5},
-                      {0.5, 0.5, -1.0, 1.0},
-                      {-1.375, 0.875, 0.0, -0.5}};
-
-  Transformation t1(m, invm);
-
-  // Test is_consistent
-  test_consistency(t1);
-
-  // Test is_close
   Transformation t2(m, invm);
-  test_closeness(t1, t2);
-
   Transformation t3(m, invm);
   t3.m[2][2] += 1.0;
-  test_not_closeness(t1, t3);
-
   Transformation t4(m, invm);
   t4.invm[2][2] += 1.0;
-  test_not_closeness(t1, t4);
-  
 
-  // Test operator*
+  REQUIRE(t.is_close(t2));
+  REQUIRE(!t.is_close(t3));
+  REQUIRE(!t.is_close(t4));
+}
+
+TEST_CASE("Transformation operator* (Transformation*Transformation)",
+          "[transformation]") {
 
   float m2[4][4] = {{3.0, 5.0, 2.0, 4.0},
                     {4.0, 1.0, 0.0, 5.0},
                     {6.0, 3.0, 2.0, 0.0},
                     {1.0, 4.0, 2.0, 1.0}};
-  
-  float invm2[4][4] = {{0.4, -0.2, 0.2, -0.6},
-                      {2.9, -1.7, 0.2, -3.1},
-                      {-5.55, 3.15, -0.4, 6.45},
-                      {-0.9, 0.7, -0.2, 1.1}};
 
-  Transformation t5(m2, invm2);
-  test_consistency(t5);
+  float invm2[4][4] = {{0.4, -0.2, 0.2, -0.6},
+                       {2.9, -1.7, 0.2, -3.1},
+                       {-5.55, 3.15, -0.4, 6.45},
+                       {-0.9, 0.7, -0.2, 1.1}};
+
+  Transformation t2(m2, invm2);
+  REQUIRE(t2.is_consistent());
 
   float m3[4][4] = {{33.0, 32.0, 16.0, 18.0},
                     {89.0, 84.0, 40.0, 58.0},
                     {118.0, 106.0, 48.0, 88.0},
                     {63.0, 51.0, 22.0, 50.0}};
-  
+
   float invm3[4][4] = {{-1.45, 1.45, -1.0, 0.6},
-                      {-13.95, 11.95, -6.5, 2.6},
-                      {25.525, -22.025, 12.25, -5.2},
-                      {4.825, -4.325, 2.5, -1.1}};
-  
-  Transformation t6(m3, invm3);
-  test_consistency(t6);
-  test_closeness(t6, (t1*t5), "operator* (Transformation*Transformation)");
-  
-  float m4[4][4] = {{1.0, 2.0, 3.0, 4.0},
+                       {-13.95, 11.95, -6.5, 2.6},
+                       {25.525, -22.025, 12.25, -5.2},
+                       {4.825, -4.325, 2.5, -1.1}};
+
+  Transformation t3(m3, invm3);
+  REQUIRE(t3.is_consistent());
+
+  REQUIRE(t3.is_close(t * t2));
+}
+
+TEST_CASE("Transformation operator*", "[transformation]") {
+
+  float m2[4][4] = {{1.0, 2.0, 3.0, 4.0},
                     {5.0, 6.0, 7.0, 8.0},
                     {9.0, 9.0, 8.0, 7.0},
                     {0.0, 0.0, 0.0, 1.0}};
-  
-  float invm4[4][4] = {{-3.75, 2.75, -1, 0},
-                      {5.75, -4.75, 2.0, 1.0},
-                      {-2.25, 2.25, -1.0, -2.0},
-                      {0.0, 0.0, 0.0, 1.0}};
 
-  Transformation t7(m4, invm4);
-  test_consistency(t7);
+  float invm2[4][4] = {{-3.75, 2.75, -1, 0},
+                       {5.75, -4.75, 2.0, 1.0},
+                       {-2.25, 2.25, -1.0, -2.0},
+                       {0.0, 0.0, 0.0, 1.0}};
+
+  Transformation t2(m2, invm2);
+  REQUIRE(t2.is_consistent());
 
   Vec v(14.0, 38.0, 51.0);
+  REQUIRE(v.is_close(t2 * Vec(1.0, 2.0, 3.0)));
 
-  if (v.is_close(t7 * Vec(1.0, 2.0, 3.0)) == 0) {
-    cout << "Error: operator* (Transformation*Vec)." << endl;
-    abort();
-  }
-  
   Point p(18.0, 46.0, 58.0);
-
-  if (p.is_close(t7 * Point(1.0, 2.0, 3.0)) == 0) {
-    cout << "Error: operator* (Transformation*Point)." << endl;
-    abort();
-  }
+  REQUIRE(p.is_close(t2 * Point(1.0, 2.0, 3.0)));
 
   Normal n(-8.75, 7.75, -3.0);
-
-  if (n.is_close(t7 * Normal(3.0, 2.0, 4.0)) == 0) {
-    cout << "Error: operator* (Transformation*Normal)." << endl;
-    abort();
-  }
-
-
-  // Test inverse
-
-  Transformation t8 = t1.inverse();
-  
-  test_consistency(t8, "inverse()");
-
-  Transformation prod = t1 * t8;
-  test_consistency(prod, "operator* with inverse()");
-  test_not_closeness(prod, t1, "operator* with inverse()");
-
-  // Test translation
-
-  Transformation tr1 = translation(Vec(1.0, 2.0, 3.0));
-  test_consistency(tr1, "translation()");
-  
-  Transformation tr2 = translation(Vec(4.0, 6.0, 8.0));
-  test_consistency(tr2, "translation()");
-        
-  Transformation prod2 = tr1 * tr2;
-  test_consistency(prod2, "operator* with translation()");
-  
-  Transformation tr3 = translation(Vec(5.0, 8.0, 11.0));
-  test_closeness(prod2, tr3,"operator* with translation()");
-  
-  // Test scaling
-  
-  Transformation sc1=scaling(Vec(2.0,5.0,10.0));
-  test_consistency(sc1, "scaling()");
-  
-  Transformation sc2=scaling(Vec(3.0,2.0,4.0));
-  test_consistency(sc2, "scaling()");
-  
-  Transformation sc3=scaling(Vec(6.0,10.0,40.0));
-  Transformation prod3=sc1*sc2;
-  test_closeness(prod3, sc3,"operator* with scaling()");
-
-// Test rotations
-
-Transformation rx1=rotation_x(0.1);
-Transformation ry1=rotation_y(0.1);
-Transformation rz1=rotation_z(0.1);
-  
-  test_consistency(rx1, "rotation_x()");
-  test_consistency(ry1, "rotation_y()");
-  test_consistency(rz1, "rotation_z()");
-  
-  Transformation rx2=rotation_x(90.0);
-  Transformation ry2=rotation_y(90.0);
-  Transformation rz2=rotation_z(90.0);
-
-// da sistemare
-if ((rx2*VEC_Y).is_close(VEC_Z) == 0 | (ry2*VEC_Z).is_close(VEC_X) == 0 | (rz2*VEC_X).is_close(VEC_Y) == 0) {
-  cout << "Error: rotation_*()." << endl;
-  abort();
+  REQUIRE(n.is_close(t2 * Normal(3.0, 2.0, 4.0)));
 }
 
-return 0;
+TEST_CASE("Transformation inverse", "[transformation]") {
+
+  Transformation t2 = t.inverse();
+  REQUIRE(t2.is_consistent());
+
+  Transformation prod = t * t2;
+  REQUIRE(prod.is_consistent());
+
+  REQUIRE(!prod.is_close(t));
+}
+
+TEST_CASE("Transformation translation", "[transformation]") {
+
+  Transformation tr1 = translation(Vec(1.0, 2.0, 3.0));
+  REQUIRE(tr1.is_consistent());
+
+  Transformation tr2 = translation(Vec(4.0, 6.0, 8.0));
+  REQUIRE(tr2.is_consistent());
+
+  Transformation prod = tr1 * tr2;
+  REQUIRE(prod.is_consistent());
+
+  Transformation tr3 = translation(Vec(5.0, 8.0, 11.0));
+  REQUIRE(prod.is_close(tr3));
+}
+
+TEST_CASE("Transformation scaling", "[transformation]") {
+
+  Transformation sc1 = scaling(Vec(2.0, 5.0, 10.0));
+  REQUIRE(sc1.is_consistent());
+
+  Transformation sc2 = scaling(Vec(3.0, 2.0, 4.0));
+  REQUIRE(sc2.is_consistent());
+
+  Transformation sc3 = scaling(Vec(6.0, 10.0, 40.0));
+  Transformation prod3 = sc1 * sc2;
+
+  REQUIRE(prod3.is_close(sc3));
+}
+
+TEST_CASE("Transformation rotations", "[transformation]") {
+
+  Transformation rx1 = rotation_x(0.1);
+  Transformation ry1 = rotation_y(0.1);
+  Transformation rz1 = rotation_z(0.1);
+
+  REQUIRE(rx1.is_consistent());
+  REQUIRE(ry1.is_consistent());
+  REQUIRE(rz1.is_consistent());
+
+  Transformation rx2 = rotation_x(90.0);
+  Transformation ry2 = rotation_y(90.0);
+  Transformation rz2 = rotation_z(90.0);
+
+  REQUIRE((rx2 * VEC_Y).is_close(VEC_Z));
+  REQUIRE((ry2 * VEC_Z).is_close(VEC_X));
+  REQUIRE((rz2 * VEC_X).is_close(VEC_Y));
 }
