f3ba7470f0d431bb4a0d1a15445dcfcbb736e954
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-05-29 13:33:48 +0200

    Add Plane, check_if_intersection, documentation in shapes.h

--- include/shapes.h
+++ include/shapes.h
@@ -9,24 +9,72 @@
 Vec2d _sphere_point_to_uv(Point);
 Normal _sphere_normal(Point, Vec);
 
+/**
+ * A generic 3D shape (sphere, plane...)
+ *
+ * It is an abstract stuct
+ * with virtual methods ray_intersection(Ray) to be implemented in derived structs.
+ */
 struct Shape{
   virtual HitRecord ray_intersection(Ray) = 0;
+  virtual bool check_if_intersection(Ray) = 0;
 };
 
 //Se il vostro linguaggio lo supporta, il tipo di ritorno dovrebbe essere nullable.
+/**
+ * A 3D unit sphere centered on the origin
+ *
+ * @param transformation changes properties of the unit sphere, through rescaling and translating it.
+ */
 struct Sphere : public Shape {
   
   Transformation transformation;
+  // Material material;
 
-  Sphere(Transformation t = Transformation()) : transformation{t} {}
+  Sphere(Transformation t = Transformation()) : transformation{t} {} // da aggiungere material come trasnformation
   
+  /**
+   Checks if the given ray hits the sphere
+   @param ray Input ray to check
+   @return HitRecord struct containing all infos about the ray-shape intersection (param 'init' set to faulse if no intersection happens)
+   */
   HitRecord ray_intersection(Ray);
+  
+  /**
+   Checks if the given ray hits the sphere
+   @param ray Input ray to check
+   @return boolean value
+   */
+  bool check_if_intersection(Ray);
   
 };
 
-/*struct Plane : public Shape {
+/**
+ * A xy infinite plane
+ *
+ * @param transformation changes properties of the unit sphere, through translating and rotating it.
+ */
+
+struct Plane : public Shape {
+  Transformation transformation;
   
-};*/
+  Plane(Transformation t = Transformation()) : transformation{t} {}
+  
+  /**
+   Checks if the given ray hits the plane
+   @param ray Input ray to check
+   @return HitRecord struct containing all infos about the ray-shape intersection (param 'init' set to faulse if no intersection happens)
+   */
+  HitRecord ray_intersection(Ray);
+  
+  /**
+   Checks if the given ray hits the sphere
+   @param ray Input ray to check
+   @return boolean value
+   */
+  bool check_if_intersection(Ray);
+  
+};
 
 
 #endif
