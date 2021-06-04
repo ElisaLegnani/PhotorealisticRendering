acf2a80a0798cfd9c9277782b641167080de5c03
Elisa Legnani <elegnani97@gmail.com>
2021-06-04 11:53:43 +0200

    Change generate-animation.sh running parallel

--- examples/generate-animation.sh
+++
@@ -1,7 +0,0 @@
-for angle in $(seq 0 359); do
-    angleNNN=$(printf "%03d" $angle)
-    ./generate-image.sh $angle
-done
-
-# -r 25: number of frames per second
-ffmpeg -r 25 -f image2 -s 680x480 -i demo/image%03d.png -vcodec libx264 -pix_fmt yuv420p demo.mp4
