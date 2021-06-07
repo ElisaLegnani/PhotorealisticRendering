f7b20f0aefd1f5ec02d1b33c8eef299a399bf58c
Elisa Legnani <elegnani97@gmail.com>
2021-06-08 00:20:32 +0200

    Add PathTracer and test

--- include/render.h
+++ include/render.h
@@ -1,7 +1,7 @@
 #include "colors.h"
+#include "materials.h"
 #include "ray.h"
 #include "world.h"
-#include "materials.h"
 
 #ifndef _render_h_
 #define _render_h_
@@ -11,11 +11,10 @@ struct Renderer {
   World world;
   Color background_color;
 
-  Renderer(World w, Color bc = BLACK): world{w}, background_color{bc} {}
+  Renderer(World w, Color bc = BLACK) : world{w}, background_color{bc} {}
 
   virtual Color operator()(Ray ray) = 0;
 };
-
 
 /**
  A renderer with
@@ -23,41 +22,89 @@ struct Renderer {
  - OFF mode (default: black)
  Used mainly for debugging porpuses
  */
-struct OnOffRenderer : public Renderer{
+struct OnOffRenderer : public Renderer {
   Color color;
-  
-  OnOffRenderer(World w, Color bc = BLACK, Color c = WHITE): Renderer(w,bc), color{c} {}
-  
-  Color operator()(Ray ray){
-    if(world.ray_intersection(ray).init){
+
+  OnOffRenderer(World w, Color bc = BLACK, Color c = WHITE)
+      : Renderer(w, bc), color{c} {}
+
+  Color operator()(Ray ray) {
+    if (world.ray_intersection(ray).init) {
       return color;
-    }else{
+    } else {
       return background_color;
     }
   }
-  
 };
 
+struct FlatRenderer : public Renderer {
 
-struct FlatRenderer : public Renderer{
-  
-  FlatRenderer(World w, Color bc = BLACK): Renderer(w,bc) {}
-  
-  Color operator()(Ray ray){
-    
+  FlatRenderer(World w, Color bc = BLACK) : Renderer(w, bc) {}
+
+  Color operator()(Ray ray) {
+
     HitRecord hit = world.ray_intersection(ray);
-    
-    if (!hit.init){
-      
+
+    if (!hit.init) {
+
       return background_color;
-  
-    }else{
-      
+
+    } else {
+
       return (hit.material.brdf->pigment->get_color(hit.surface_point) +
               hit.material.emitted_radiance->get_color(hit.surface_point));
     }
   }
 };
 
-#endif
+struct PathTracer : public Renderer {
 
+  PCG pcg;
+  int num_of_rays;
+  int max_depth;
+  int russian_roulette_limit;
+
+  PathTracer(World w, Color bc = BLACK, PCG pcg = PCG(), int nrays = 10, int maxd = 2, int rrlim = 3)
+      : Renderer(w, bc), num_of_rays{nrays}, max_depth{maxd}, russian_roulette_limit{rrlim} {}
+
+  Color operator()(Ray ray) {
+    
+    if (ray.depth > max_depth)
+      return BLACK;
+      
+    HitRecord hit = world.ray_intersection(ray);
+
+    if (!hit.init)
+      return background_color;
+
+    Material hit_material = hit.material;
+    Color hit_color = hit_material.brdf->pigment->get_color(hit.surface_point);
+    Color emitted_radiance = hit_material.emitted_radiance->get_color(hit.surface_point);
+
+    float hit_color_lum = fmax(fmax(hit_color.r, hit_color.g), hit_color.b);
+
+    // Russian roulette
+    if (ray.depth >= russian_roulette_limit){
+      if (pcg.random_float() > hit_color_lum)  
+        hit_color = hit_color * ( 1.0 / (1.0 - hit_color_lum));
+        // Keep the recursion going, but compensate for other potentially discarded rays
+      else
+        return emitted_radiance;
+        // Terminate prematurely
+    }
+
+    Color cum_radiance;
+    if (hit_color_lum > 0.0){  // Only do costly recursions if it's worth it
+      for (int i{}; i < num_of_rays; ++i){
+        Ray new_ray = hit_material.brdf->scatter_ray(pcg, hit.ray.dir, hit.world_point, hit.normal, (ray.depth + 1));
+        // Recursive call
+        Color new_radiance = (*this)(new_ray);
+        cum_radiance = cum_radiance + hit_color * new_radiance;
+      }
+    }
+
+    return emitted_radiance + cum_radiance * (1.0 / num_of_rays);
+  }
+};
+
+#endif
--- test/render.cpp
+++ test/render.cpp
@@ -21,8 +21,8 @@ THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include "render.h"
-#include "imagetracer.h"
 #include "catch_amalgamated.hpp"
+#include "imagetracer.h"
 
 #define CATCH_CONFIG_MAIN
 
@@ -56,7 +56,8 @@ TEST_CASE("FlatRenderer constructor", "[renderer]") {
 
   Color sphere_color(1.0, 2.0, 3.0);
   Sphere sphere(translation(Vec(2.0, 0.0, 0.0)) * scaling(Vec(0.2, 0.2, 0.2)),
-                Material(make_shared<DiffuseBRDF>(make_shared<UniformPigment>(sphere_color))));
+                Material(make_shared<DiffuseBRDF>(
+                    make_shared<UniformPigment>(sphere_color))));
   HdrImage image(3, 3);
   OrthogonalCamera camera;
   ImageTracer tracer(image, make_shared<OrthogonalCamera>(camera));
@@ -76,4 +77,32 @@ TEST_CASE("FlatRenderer constructor", "[renderer]") {
   REQUIRE(tracer.image.get_pixel(0, 2).is_close(BLACK));
   REQUIRE(tracer.image.get_pixel(1, 2).is_close(BLACK));
   REQUIRE(tracer.image.get_pixel(2, 2).is_close(BLACK));
+}
+
+TEST_CASE("PathTracer constructor", "[renderer]") { // Furnace test
+
+  PCG pcg;
+
+  // Run the furnace test several times using random values for the emitted
+  // radiance and reflectance
+  for (int i{}; i < 5; ++i) {
+
+    World world;
+    float emitted_radiance = pcg.random_float();
+    float reflectance = pcg.random_float() * 0.9; // Approx gets worst for reflectance->1
+    Material enclosure_material(make_shared<DiffuseBRDF>(make_shared<UniformPigment>(WHITE * reflectance)),
+        make_shared<UniformPigment>(WHITE * emitted_radiance));
+    world.add(make_shared<Sphere>(Transformation(), enclosure_material));
+
+    PathTracer path_tracer(world, BLACK, pcg, 1, 100, 101);
+
+    Ray ray(Point(0, 0, 0), Vec(1, 0, 0));
+    Color color = path_tracer(ray);
+
+    float expected = emitted_radiance / (1.0 - reflectance);
+    
+    REQUIRE(are_close(expected, color.r, 1e-3));
+    REQUIRE(are_close(expected, color.g, 1e-3));
+    REQUIRE(are_close(expected, color.b, 1e-3));
+  }
 }
