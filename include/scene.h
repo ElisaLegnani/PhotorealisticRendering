dc138e32d556e50c913e1307182f44daa8c9c53b
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-06-16 14:07:44 +0200

    Start implementation lexer

--- include/scene.h
+++ include/scene.h
@@ -16,29 +16,39 @@ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFT
 IN THE SOFTWARE.
 */
 
+#include<iostream>
+
+/*#define WHITESPACE = {" ", "\t", "\n", "\r"}
+#define SYMBOLS = {"(",")","<",">","[","]","*"}*/
+
 #ifndef _scene_h_
 #define _scene_h_
 
-enum class TokenType {
-  LITERAL_NUMBER,
-  LITERAL_STRING,
-  SYMBOL,
-  KEYWORD,
+enum class Keyword{
+  MATERIAL, PLANE, SPHERE, DIFFUSE, SPECULAR, UNIFORM, CHECKERED, IMAGE, IDENTITY, TRANSLATION, ROTATION_X, ROTATION_Y, ROTATION_Z, SCALING, CAMERA, ORTHOGONAL, PERSPECTIVE, FLOAT,
 };
+
+enum class TokenType {
+  LITERAL_NUMBER, LITERAL_STRING, SYMBOL, KEYWORD, IDENTIFIER, STOPTOKEN,
+};
+
 
 union TokenValue {
   float number;
   string string;
+  char symbol;
+  Keyword key;
 
   TokenValue() : number{0.0} {}
   ~TokenValue() {}
 }:
 
 struct Token {
+  SourceLocation location;
   TokenType type;
   TokenValue value;
 
-  Token() : type(TokenType::LITERAL_NUMBER) {}
+  Token(SourceLocation loc) : location{loc} {}
 
   void assign_number(float val) {
       type = TokenType::LITERAL_NUMBER;
@@ -49,6 +59,92 @@ struct Token {
       type = TokenType::LITERAL_STRING;
       value.string = s;
   }
+  
+  void assign_symbol(char sym) {
+      type = TokenType::SYMBOL;
+      value.symbol = sym;
+  }
+  
+  void assign_keyword(Keyword keyword) {
+      type = TokenType::KEYWORD;
+      value.key = keyword;
+  }
+  
+  void assign_identifier(const string & s) {
+      type = TokenType::IDENTIFIER;
+      value.string = s;
+  }
+  
+  void assign_stoptoken() {
+      type = TokenType::STOPTOKEN;
+  }
+  
+};
+
+struct SourceLocation {
+  string file_name{""};
+  int line_num{};
+  int col_num{};
+};
+
+struct ParserError{
+  SourceLocation location;
+  string message;
+};
+
+struct InputStream{
+  
+  istream stream_in;
+  SourceLocation location;
+  int tabulations;
+  char saved_char = "";
+  SourceLocation saved_location;
+  Token saved_token;
+  
+  InuputStream(string file, int tab = 8) : location{file, 1, 1}, tabulations{tab} , saved_location{location} {} //?
+   
+  void update_location(char character){
+    if(character == "") return;
+    else if (character == "\n"){
+      location.line_num += 1;
+      location.col_num += 1;
+    }else if(character =="\t"){
+      location.col_num += tabulations;
+    }else{
+      col_num += 1;
+    }
+  }
+  
+  char read_character(){
+    
+    char ch;
+    if(saved_char != ""){
+      ch{saved_char};
+      saved_char = "";
+    }else{
+      stream_in >> ch;
+    }
+    saved_location = location;
+    update_location(ch);
+    
+    return ch;
+  }
+  
+  void unread_character(char ch){
+    if(saved_char == "") return;
+    saved_char{ch};
+    location{saved_location}
+  }
+  
+  void skip_whitespaces(){
+    char ch = read_character();
+    while(ch == " " or ch == "\t" or ch == "\n" or ch == "\r"){
+      ch = read_character();
+      if(ch == "") return;
+    }
+    unread_character(ch);
+  }
+  
 };
 
 #endif
