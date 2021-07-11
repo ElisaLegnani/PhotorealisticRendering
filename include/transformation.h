be6230a8f7d65b6ef796fe808b10ebb68ab57c8b
adelezaini <59051647+adelezaini@users.noreply.github.com>
2021-07-11 12:23:57 +0200

    Add documentation to transformation.h

--- include/transformation.h
+++ include/transformation.h
@@ -28,13 +28,21 @@ IN THE SOFTWARE.
 #define VEC_Y Vec(0.0, 1.0, 0.0)
 #define VEC_Z Vec(0.0, 0.0, 1.0)
 
-//–––––––––––––––––––––– Struct Transformation –––––––––––––––––––––––––––––––––––
+//–––––––––––––––––––––– Functions for Transformation –––––––––––––––––––––––––––––––––––
 
 bool are_matr_close(float [4][4], float [4][4]);
 
 void matr_prod(const float [4][4], const float [4][4], float [4][4]);
 
 
+//–––––––––––––––––––––– Struct Transformation –––––––––––––––––––––––––––––––––––
+/**
+ * A struct reprenting an affine transformation.
+ * It is the output when calling a 'translation', 'scaling' or 'rotation' function.
+ *
+ * @param m transformation matrix
+ * @param invm inverse transformation matrix, implemented to increase the code efficiency
+ */
 struct Transformation {
 
   float m[4][4] = {{1.0, 0.0, 0.0, 0.0},
@@ -51,12 +59,25 @@ struct Transformation {
 
   Transformation(float [4][4], float [4][4]);
   
+  /**
+   * Return a printable strings showing the transformation matrix content
+   */
   string get_string();
 
+  /**
+   * Check if the transformation can be considered the same as the given one
+   * Needed to avoid floating-point approximation limits.
+   */
   bool is_close(Transformation);
   
+  /**
+   * Check if the transformation is consistent ( m X invm = identity_matrix )
+   */
   bool is_consistent();
 
+  /**
+   * Retrun the inverse transformation ( i.e. swap 'm' and 'invm' )
+   */
   Transformation inverse();
 
 };
@@ -68,10 +89,44 @@ Vec operator*(Transformation, Vec);
 Normal operator*(Transformation, Normal);
 
 //–––––––––––––– Transformations –––––––––––––––
+/**
+ * Translate of a given 'Vec' oriented in 3D space
+ *
+ * @param v 'Vec' object generating the translation
+ * @return 'Transformation' object representing the translation
+ */
 Transformation translation(Vec);
+
+/**
+ * Rescale of a given 'Vec'
+ *
+ * @param v 'Vec' object generating the scaling
+ * @return 'Transformation' object representing the scaling
+ */
 Transformation scaling(Vec);
+
+/**
+ * Rotate around x-axis of a given angle
+ *
+ * @param angle_deg floating-point number reprensenting the angle in degrees
+ * @return 'Transformation' object representing the x-rotation
+ */
 Transformation rotation_x(float);
+
+/**
+ * Rotate around y-axis of a given angle
+ *
+ * @param angle_deg floating-point number reprensenting the angle in degrees
+ * @return 'Transformation' object representing the y-rotation
+ */
 Transformation rotation_y(float);
+
+/**
+ * Rotate around z-axis of a given angle
+ *
+ * @param angle_deg floating-point number reprensenting the angle in degrees
+ * @return 'Transformation' object representing the z-rotation
+ */
 Transformation rotation_z(float);
 
 #endif
